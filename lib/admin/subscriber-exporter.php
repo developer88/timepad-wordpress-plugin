ebac249d55cada6071d5af31bc10fe0de11240ac
Andrey Eremin <andrey.eremin@xing.com>
2018-12-20 18:07:30 +0100

    feat: export subscribers

--- lib/admin/subscriber-exporter.php
+++ lib/admin/subscriber-exporter.php
@@ -17,60 +17,63 @@ if ( ! class_exists( 'TimepadEvents_Admin_Subscribers_Exporter' ) ) :
          * @return null
          */
         public static function export( $event ) {
-            //TimepadEvents_Helpers::debug('Event id=' . $event['id']);
+            TimepadEvents_Helpers::debug('__DIR__= ' . __DIR__);   
+            TimepadEvents_Helpers::debug('plugin_dir_path( __DIR__ )= ', plugin_dir_path( __DIR__ )); 
+            TimepadEvents_Helpers::debug('Exporting subscribers. Getting orders');
+
             $orders = $this->_get_request_array( str_replace('{event_id}', $event['id'], $this->_config['orders_url']), 'get' );
+            TimepadEvents_Helpers::debug('Exporting subscribers. Received orders');
+            TimepadEvents_Helpers::debug($orders);
+            $controls = new NewsletterControls();
+            $module = NewsletterUsers::instance();
+            $options_profile = get_option('newsletter_profile');            
+            TimepadEvents_Helpers::debug('Exporting subscribers. Processing orders');
+            foreach ($orders['values'] as $order) {
+                if( self::order_complete($order) === true ) {
+                    self::export_subscriber($order, $module);
+                }
+            }
         }
 
+        public static function export_subscriber( $order, $module ) {
+            foreach ($order['tickets'] as $ticket) {
+                if (empty($email)) {
+                    TimepadEvents_Helpers::debug('Exporting subscribers. Empty email');  
+                    continue;
+                }
+                $first_name = $ticket['answers']['name'];
+                $last_name = $ticket['answers']['surname'];
+                $email = $newsletter->normalize_email($ticket['answers']['mail']);
 
+                if (!$newsletter->is_email($email)) {
+                    TimepadEvents_Helpers::debug('Exporting subscribers. Invalid email' . $email);  
+                    continue;
+                }
 
-        // public function export_subscribers( $event ) {
-        //     TimepadEvents_Helpers::debug('Event id=' . $event['id']);
-        //     TimepadEvents_Helpers::debug(print_r($event, true));
-        //     $orders = $this->_get_request_array( str_replace('{event_id}', $event['id'], $this->_config['orders_url']), 'get' );
-        //     foreach ($orders['values'] as $order) {
-        //         if ( isset($order['mail']) && strlen($order['mail']) > 0 && $order['status']['name'] == NUL!!!! ) {
-        //             // TODO: save $order['mail'] if new
-        //             TimepadEvents_Helpers::debug('__DIR__= ' . __DIR__);   
-        //             TimepadEvents_Helpers::debug('plugin_dir_path( __DIR__ )= ', plugin_dir_path( __DIR__ ));               
-        //             //require_once plugin_dir_path( __DIR__ ) . '/newsletter/includes/controls.php';
-        // //             $controls = new NewsletterControls();
-        // //             $module = NewsletterUsers::instance();
-        // //             $options_profile = get_option('newsletter_profile');
-        // //             // Builds a subscriber data structure
-        // // $email = $newsletter->normalize_email($data[0]);
-        // // if (empty($email)) {
-        // //     continue;
-        // // }
+                $subscriber = $module->get_user($email, ARRAY_A);
+                if ($subscriber != null) {
+                    TimepadEvents_Helpers::debug('Exporting subscribers. User exists for ' . $email); 
+                    continue;
+                }
 
-        // // if (!$newsletter->is_email($email)) {
-        // //     $results .= '[INVALID EMAIL] ' . $line . "\n";
-        // //     $error_count++;
-        // //     continue;
-        // // }
+                $subscriber = array();
+                $subscriber['email'] = $email;
+                if (isset($first_name)) {
+                    $subscriber['name'] = $module->normalize_name($first_name);
+                }
+                if (isset($last_name)) {
+                    $subscriber['surname'] = $module->normalize_name($last_name);
+                }
+                // S - not confirmed, C - confirmed
+                $subscriber['status'] = 'S';
+                TimepadEvents_Helpers::debug('Exporting subscribers. Saving user with email ' . $email); 
+                $module->save_user($subscriber);
+            }            
+        }
 
-        // // $subscriber = $module->get_user($email, ARRAY_A);
-        // // if ($subscriber == null) {
-        // //     $subscriber = array();
-        // //     $subscriber['email'] = $email;
-        // //     if (isset($data[1])) {
-        // //         $subscriber['name'] = $module->normalize_name($data[1]);
-        // //     }
-        // //     if (isset($data[2])) {
-        // //         $subscriber['surname'] = $module->normalize_name($data[2]);
-        // //     }
-        // //     if (isset($data[3])) {
-        // //         $subscriber['sex'] = $module->normalize_sex($data[3]);
-        // //     }
-        // //     $subscriber['status'] = $controls->data['import_as'];
-        // //     foreach ($controls->data['preferences'] as $i) {
-        // //         $subscriber['list_' . $i] = 1;
-        // //     }
-        // //     $module->save_user($subscriber);
-        // //     $results .= '[ADDED] ' . $line . "\n";
-        // //     $added_count++;
-        //         }
-        //     }
-        // }
+        public static function order_complete ( $order ) {
+            // TODO!!
+        }
 
     }
 
