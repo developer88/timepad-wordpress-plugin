41b00986a682c3b658c4c09aaed288ec8d617b2c
Andrey Eremin <andrey.eremin@xing.com>
2019-04-02 09:08:19 +0200

    fix: add initializer to PostDescription

--- lib/admin/post-description.php
+++ lib/admin/post-description.php
@@ -4,7 +4,25 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if ( ! class_exists( 'TimepadEvents_Admin_Post_Description' ) ) :
     
     class TimepadEvents_Admin_Post_Description extends TimepadEvents_Admin_Base {
-    
+
+        /**
+         * @var array The plugin data array
+         */
+        protected $_event = array();
+
+        /**
+         * Initializes the class
+         * 
+         * @since  1.1.5.1000
+         * @param  array $event
+         * @access public
+         * @return object 
+         */
+        function __construct( $event ) {
+            $this->_event = $event;
+            parent::__construct();
+        }
+
         /**
          * Returns description with all necessary information 
          *   from timePad event
@@ -14,33 +32,33 @@ if ( ! class_exists( 'TimepadEvents_Admin_Post_Description' ) ) :
          * @access public
          * @return string Post description with details
          */
-        public static function render( $event ) {
+        public function render() {
             $content = '';
-            if ( isset( $event['description_short'] ) && !empty( $event['description_short'] ) ) {
-                $content .= $event['description_short'];
+            if ( isset( $this->_event['description_short'] ) && !empty( $this->_event['description_short'] ) ) {
+                $content .= $this->_event['description_short'];
             }
-            if ( isset( $event['description_html'] ) && !empty( $event['description_html'] ) ) {
-                $content .= $event['description_html'];
+            if ( isset( $this->_event['description_html'] ) && !empty( $this->_event['description_html'] ) ) {
+                $content .= $this->_event['description_html'];
             }
             if ( !empty( $content ) ) {
-                $content .= self::render_details($event);
+                $content .= self::render_details();
             }
 
             return $content;
         }
 
-        public static function render_details( $event ) {
+        public static function render_details() {
             $content = '<div class="timepad-event-details">';
                 $content .= '<div class="timepad-event-details-title">Детали события</div>';
-                $content .= self::render_date($event);
-                $content .= self::render_location($event);
+                $content .= self::render_date();
+                $content .= self::render_location();
             $content .= '</div>';
 
             return $content;
         }
 
-        public static function render_date( $event ) {
-            $moscow_time = self::moscow_date_time_str( $event );
+        public static function render_date() {
+            $moscow_time = self::moscow_date_time_str();
             $content .= '<div class="timepad-event-details-date">';
                 $content .= '<i class="font-icon-post fa fa-clock-o"></i> ';
                 $content .= '<span>' . date("j.m.o", $moscow_time['starts_at']);
@@ -55,11 +73,11 @@ if ( ! class_exists( 'TimepadEvents_Admin_Post_Description' ) ) :
             return $content;
         }
 
-        public static function moscow_date_time_str( $event ) {
+        public static function moscow_date_time_str() {
             $moscow_offset = '3 hours';
-            $starts_at_moscow = strtotime($event['starts_at'] . ' + ' . $moscow_offset);
-            $ends_at_moscow = strtotime($event['ends_at'] . ' + ' . $moscow_offset);
-            $use_end_time = (!empty( $event['ends_at'] ) && $starts_at_moscow != $ends_at_moscow);
+            $starts_at_moscow = strtotime($this->_event['starts_at'] . ' + ' . $moscow_offset);
+            $ends_at_moscow = strtotime($this->_event['ends_at'] . ' + ' . $moscow_offset);
+            $use_end_time = (!empty( $this->_event['ends_at'] ) && $starts_at_moscow != $ends_at_moscow);
             
             return array(
                 'starts_at' => $starts_at_moscow,
@@ -68,8 +86,8 @@ if ( ! class_exists( 'TimepadEvents_Admin_Post_Description' ) ) :
             );
         }
 
-        public static function render_location( $event ) {
-            $location_string = implode( ', ', array($event['location']['country'], $event['location']['city'], $event['location']['address']) );  
+        public static function render_location() {
+            $location_string = implode( ', ', array($this->_event['location']['country'], $this->_event['location']['city'], $this->_event['location']['address']) );  
             $content = '<div class="timepad-event-details-location">';
                 $content .= '<div class="timepad-event-details-location-address"><i class="font-icon-post fa fa-home"></i> ' . $location_string . '</div>';
                 $content .= '<div class="timepad-event-details-location-map">';
